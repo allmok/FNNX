bb18f4648571d56eb8912d95ffea4e64b7fba949
Oleh Kostromin <kostromin97@gmail.com>
2025-09-13 15:52:13 +0200

    initial implementation of stdio handler

--- src/python/fnnx/utils.py
+++ src/python/fnnx/utils.py
@@ -1,6 +1,7 @@
 from asyncio import events
 import functools
 import contextvars
+import sys
 
 
 async def to_thread(executor, func, /, *args, **kwargs):
@@ -8,3 +9,9 @@ async def to_thread(executor, func, /, *args, **kwargs):
     ctx = contextvars.copy_context()
     func_call = functools.partial(ctx.run, func, *args, **kwargs)
     return await loop.run_in_executor(executor, func_call)
+
+
+def get_python_version(micro: bool = True) -> str:
+    if micro:
+        return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+    return f"{sys.version_info.major}.{sys.version_info.minor}"
